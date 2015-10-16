0dc54f95fbca34357bf475196fa150b246556d6e
tracedeng <1401733434@qq.com>
2015-10-16 17:32:56 +0800

    升级用户测试

--- test/consumer_test.py
+++ test/consumer_test.py
@@ -5,23 +5,22 @@ __author__ = 'tracedeng'
 import socket
 import common_pb2
 import package
+
+import calculus.wrapper.log as log
+g_log = log.WrapperLog('stream', name=__name__, level=log.DEBUG).log  # 启动日志功能
 from branch_socket import receive_from_sock
 from branch_socket import send_to_sock
 
-address = ('127.0.0.1', 9527)
 
-
-def consumer_create_test(phone_number='18688982240'):
-    global address
+def consumer_create_test(numbers='18688982240'):
     req = common_pb2.Request()
     req.head.cmd = 100
     req.head.seq = 2
-    req.head.phone_number = phone_number
-    # req.consumer_create_request.phone_number = "18688982240"
+    req.head.numbers = numbers
     material = req.consumer_create_request.material
-    req.consumer_create_request.material.phone_number = phone_number
+    req.consumer_create_request.material.numbers = numbers
     req.consumer_create_request.material.nickname = "tracedengtracedengtracedengtracedengtracedengtracedeng"
-    req.consumer_create_request.material.sexy = 1
+    req.consumer_create_request.material.sexy = "male"
     material.introduce = "i introduce myself"
     material.email = "18688982240@qq.com"
     material.age = 18
@@ -32,23 +31,21 @@ def consumer_create_test(phone_number='18688982240'):
 
 
 def consumer_retrieve_test():
-    global address
     req = common_pb2.Request()
     req.head.cmd = 101
     req.head.seq = 2
-    req.head.phone_number = "18688982243"
-    req.consumer_retrieve_request.phone_number = "18688982243"
+    req.head.numbers = "18688982243"
+    req.consumer_retrieve_request.numbers = "18688982243"
 
     pack_send_receive(req)
 
 
 def consumer_update_test():
-    global address
     req = common_pb2.Request()
     req.head.cmd = 103
     req.head.seq = 2
-    req.head.phone_number = "18688982243"
-    req.consumer_update_request.phone_number = "18688982243"
+    req.head.numbers = "18688982243"
+    req.consumer_update_request.numbers = "18688982243"
     req.consumer_update_request.material.nickname = "dudu"
     req.consumer_update_request.material.age = 1000
 
@@ -56,45 +53,52 @@ def consumer_update_test():
 
 
 def consumer_delete_test():
-    global address
     req = common_pb2.Request()
     req.head.cmd = 104
     req.head.seq = 2
-    req.head.phone_number = "18688982243"
-    req.consumer_delete_request.phone_number = "18688982243"
+    req.head.numbers = "18688982243"
+    req.consumer_delete_request.numbers = "18688982243"
 
     pack_send_receive(req)
 
 
 def pack_send_receive(req):
-    global address
+    address = ('127.0.0.1', 9527)
     request = package.serial_pb(req)
 
-    # send_to_address(request, address)
+    # 发包
     sock = socket.socket(type=socket.SOCK_DGRAM)
     sock.connect(address)
-    # print('Sending %s bytes to %s:%s' % ((len(request), ) + address))
-    # print('%s' % req)
-    # sock.send(request)
+    g_log.debug("Sending %s bytes to %s:%s", len(request), address)
+    g_log.debug("%s", req)
     send_to_sock(sock, request)
 
+    # 收包
     result = receive_from_sock(sock)
     if result == 0:
         return 0
     response, _ = result
-    # print('receive %s bytes from %s:%s' % ((len(response), ) + _))
+    g_log.debug('receive %s bytes from %s:%s', len(response), _)
+
     res = common_pb2.Response()
     res.ParseFromString(response[6:-2])
-    # print('%s' % res)
+    g_log.debug('%s', res)
+
+
+def batch_test_create():
+    import time
+    begin = 15100001111
+    last = 15100001111
+    start = time.clock()
+    for i in xrange(begin, last):
+        consumer_create_test(str(i))
+    end = time.clock()
+    g_log.debug("create %s consumer, cost %s cpu time", last - begin, end-start)
+
 
 if "__main__" == __name__:
     # consumer_delete_test()
-    import time
-    start = time.clock()
-    for i in xrange(15100001111, 15100101111):
-        consumer_create_test(str(i))
-    end = time.clock()
-    print end-start
     # consumer_retrieve_test()
     # consumer_update_test()
     # consumer_retrieve_test()
+    pass
