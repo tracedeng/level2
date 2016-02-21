4b084cbaa7d9553538b2aaf3dfe890a9bb4d3229
tracedeng <1401733434@qq.com>
2016-02-21 15:40:36 +0800

    启动上报

--- lv2/maintain.py
+++ lv2/maintain.py
@@ -11,8 +11,6 @@ import log
 g_log = log.WrapperLog('stream', name=__name__, level=log.DEBUG).log  # 启动日志功能
 from account_valid import account_is_valid_consumer, account_is_valid
 from account_auxiliary import verify_session_key, identity_to_numbers
-from merchant import user_is_merchant_manager, merchant_retrieve_with_merchant_identity_only, \
-    merchant_material_copy_from_document
 
 
 class Maintain():
@@ -37,14 +35,15 @@ class Maintain():
         :return: 0/不回包给前端，pb/正确返回，timeout/超时
         """
         try:
-            # 验证登录态，某些命令可能不需要登录态，此处做判断
-            code, message = verify_session_key(self.numbers, self.session_key)
-            if 10400 != code:
-                g_log.debug("verify session key failed, %s, %s", code, message)
-                return package.error_response(self.cmd, self.seq, 90001, "invalid session key")
+            if self.cmd != 902:
+                # 验证登录态，某些命令可能不需要登录态，此处做判断
+                code, message = verify_session_key(self.numbers, self.session_key)
+                if 10400 != code:
+                    g_log.debug("verify session key failed, %s, %s", code, message)
+                    return package.error_response(self.cmd, self.seq, 90001, "invalid session key")
 
-            command_handle = {901: self.version_report, 902: self.merchant_retrieve_voucher,
-                              903: self.confirm_voucher, 904: self.feed_back}
+            command_handle = {901: self.version_report, 902: self.boot_report,
+                              903: self.active_report, 904: self.feed_back}
 
             result = command_handle.get(self.cmd, self.dummy_command)()
             if result == 0:
@@ -99,26 +98,26 @@ class Maintain():
             g_log.error("%s", e)
             return 0
 
-    def merchant_retrieve_voucher(self):
+    def boot_report(self):
         """
-        商家读取优惠券
+        启动上报
         :return:
         """
         try:
-            body = self.request.merchant_retrieve_voucher_request
-            numbers = body.numbers
-            merchant_identity = body.merchant_identity
-            identity = body.identity
+            body = self.request.boot_report_request
+            # numbers = body.numbers
+            version = body.version
+            # identity = body.identity
 
-            if not numbers:
-                # 根据包体中的merchant_identity获取numbers
-                code, numbers = identity_to_numbers(identity)
-                if code != 10500:
-                    self.code = 90201
-                    self.message = "missing argument"
-                    return 1
+            # if not numbers:
+            #     # 根据包体中的merchant_identity获取numbers
+            #     code, numbers = identity_to_numbers(identity)
+            #     if code != 10500:
+            #         self.code = 90201
+            #         self.message = "missing argument"
+            #         return 1
 
-            self.code, self.message = merchant_retrieve_voucher(numbers, merchant_identity)
+            self.code, self.message = boot_report(version)
 
             if 90200 == self.code:
                 # 更新成功
@@ -126,12 +125,7 @@ class Maintain():
                 response.head.cmd = self.head.cmd
                 response.head.seq = self.head.seq
                 response.head.code = 1
-                response.head.message = "merchant retrieve voucher done"
-
-                vouchers = response.merchant_retrieve_voucher_response.vouchers
-                for value in self.message:
-                    vouchers_one = vouchers.add()
-                    voucher_copy_from_document(vouchers_one, value)
+                response.head.message = "boot report done"
 
                 return response
             else:
@@ -140,19 +134,16 @@ class Maintain():
             g_log.error("%s", e)
             return 0
 
-    def confirm_voucher(self):
+    def active_report(self):
         """
-        商家确认优惠券
+        活跃上报
         :return:
         """
         try:
-            body = self.request.confirm_voucher_request
+            body = self.request.active_report_request
             numbers = body.numbers
             identity = body.identity
-            c_numbers = body.c_numbers
-            c_identity = body.c_identity
-            merchant_identity = body.merchant_identity
-            voucher_identity = body.voucher_identity
+            mode = body.mode
 
             if not numbers:
                 # 根据包体中的merchant_identity获取numbers
@@ -162,17 +153,7 @@ class Maintain():
                     self.message = "missing argument"
                     return 1
 
-            if not c_numbers:
-                # 根据包体中的merchant_identity获取numbers
-                code, c_numbers = identity_to_numbers(c_identity)
-                if code != 10500:
-                    self.code = 90302
-                    self.message = "missing argument"
-                    return 1
-
-            kwargs = {"manager": numbers, "numbers": c_numbers, "merchant_identity": merchant_identity,
-                      "voucher_identity": voucher_identity}
-            self.code, self.message = confirm_voucher(**kwargs)
+            self.code, self.message = active_report(numbers, mode)
 
             if 90300 == self.code:
                 # 更新成功
@@ -180,7 +161,7 @@ class Maintain():
                 response.head.cmd = self.head.cmd
                 response.head.seq = self.head.seq
                 response.head.code = 1
-                response.head.message = "manager confirm voucher done"
+                response.head.message = "active report done"
 
                 return response
             else:
@@ -277,108 +258,65 @@ def version_report(numbers, version):
         return 90115, "exception"
 
 
-def consumer_retrieve_all_voucher(numbers):
+def boot_report(numbers, version):
     """
-    用户读取优惠券
+    启动上报
     :param numbers:
+    :param version: 版本
     :return:
     """
     try:
         # 检查合法账号
-        if not account_is_valid_consumer(numbers):
-            g_log.warning("invalid customer account %s", numbers)
-            return 90116, "invalid account"
+        # if not account_is_valid(numbers):
+        #     g_log.warning("invalid customer account %s", numbers)
+        #     return 90311, "invalid account"
 
-        collection = get_mongo_collection("voucher")
+        collection = get_mongo_collection("boot")
         if not collection:
-            g_log.error("get collection voucher failed")
-            return 90117, "get collection voucher failed"
-        records = collection.find({"numbers": numbers, "used": 0}).sort("merchant_identity")
-        if not records:
-            g_log.error("retrieve voucher failed")
-            return 90118, "retrieve voucher failed"
+            g_log.error("get collection boot failed")
+            return 90213, "get collection boot failed"
 
-        return 90100, records
+        boot_time = datetime.now()
+        value = {"numbers": numbers, "version": version, "boot_time": boot_time, "total": 1}
+        boot = collection.find_one_and_update({"boot_time": boot_time, "version": version}, {"$inc": {"total": 1}},
+                                              {"$set": value})
+        if not boot:
+            g_log.error("boot report failed")
+            return 50714, "boot report failed"
+
+        return 90200, "yes"
     except Exception as e:
         g_log.error("%s", e)
-        return 90119, "exception"
+        return 90215, "exception"
 
 
-def merchant_retrieve_voucher(numbers, merchant_identity):
+def active_report(numbers, mode):
     """
-    商家读取优惠券
+    活跃上报
     :param numbers:
-    :param merchant_identity: 商家ID
-    :return:
-    """
-    try:
-        # 检查管理员和商家关系
-        merchant = user_is_merchant_manager(numbers, merchant_identity)
-        if not merchant:
-            g_log.error("%s is not merchant %s manager", numbers, merchant_identity)
-            return 90111, "not manager"
-
-        collection = get_mongo_collection("voucher")
-        if not collection:
-            g_log.error("get collection voucher failed")
-            return 90212, "get collection voucher failed"
-        records = collection.find({"merchant_identity": merchant_identity, "used": 0})
-        if not records:
-            g_log.error("retrieve voucher failed")
-            return 90213, "retrieve voucher failed"
-
-        return 90200, records
-    except Exception as e:
-        g_log.error("%s", e)
-        return 90214, "exception"
-
-
-def confirm_voucher(**kwargs):
-    """
-    商家确认优惠券
-    :param kwargs: {"numbers": 186889882240, "merchant_identity": "", "manager": "18688982240",
-                    "voucher_identity": "a1der234"}
+    :param mode: merchant or consumer
     :return:
     """
     try:
         # 检查合法账号
-        numbers = kwargs.get("numbers", "")
-        if not account_is_valid_consumer(numbers):
+        if not account_is_valid(numbers):
             g_log.warning("invalid customer account %s", numbers)
-            return 90111, "invalid account"
+            return 90311, "invalid account"
 
-        manager = kwargs.get("manager", "")
-        merchant_identity = kwargs.get("merchant_identity", "")
-        merchant = user_is_merchant_manager(manager, merchant_identity)
-        if not merchant:
-            g_log.error("%s is not merchant %s manager", manager, merchant_identity)
-            return 90312, "not manager"
-
-        voucher_identity = kwargs.get("voucher_identity", "")
-        collection = get_mongo_collection("voucher")
+        collection = get_mongo_collection("active")
         if not collection:
-            g_log.error("get collection voucher failed")
-            return 90314, "get collection voucher failed"
-        voucher = collection.find_one_and_update({"merchant_identity": merchant_identity, "numbers": numbers,
-                                                  "_id": ObjectId(voucher_identity), "used": 0}, {"$set": {"used": 1}})
-        if not voucher:
-            g_log.error("voucher %s not exist", voucher_identity)
-            return 90315, "confirm voucher failed"
+            g_log.error("get collection active failed")
+            return 90313, "get collection active failed"
 
-        # 更新记录入库
-        collection = get_mongo_collection("voucher_record")
-        if not collection:
-            g_log.error("get collection voucher record failed")
-            return 90316, "get collection voucher record failed"
-        result = collection.insert_one({"voucher_identity": voucher_identity, "time": datetime.now(),
-                                        "operator": manager})
-        if not result:
-            g_log.error("insert voucher record failed")
+        value = {"numbers": numbers, "mode": mode, "active_time": datetime.now()}
+        active_identity = collection.insert_one(value).inserted_id
+        active_identity = str(active_identity)
+        g_log.debug("insert active report %s", active_identity)
 
-        return 90300, "yes"
+        return 90300, active_identity
     except Exception as e:
         g_log.error("%s", e)
-        return 90317, "exception"
+        return 90315, "exception"
 
 
 def feed_back(**kwargs):
